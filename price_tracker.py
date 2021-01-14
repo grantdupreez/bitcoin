1cc1ed68684c0bdf3dadd185cf8db8c425f73cb1
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-14 12:30:37 +0000

    Update price_tracker.py

--- price_tracker.py
+++ price_tracker.py
@@ -1,49 +1,21 @@
-import streamlit as st
-import pandas as pd
-import base64
-from bs4 import BeautifulSoup
+import pandas
 import requests
-import json
-import time
+from bs4 import BeautifulSoup
 
-@st.cache
-def load_data():
-    url = "https://coinmarketcap.com/currencies/bitcoin/historical-data/?start="
-    start_date = "20210101&end="
-    end_date = "20210113"
-    cmc = requests.get(url + start_date + end_date)
-    soup = BeautifulSoup(cmc.content, 'html.parser')
-    data = soup.find('script', id='__NEXT_DATA__', type='application/json')
-    historical_data = json.loads(data.contents[0])
-    quotes = historical_data['props']['initialState']['cryptocurrency']['ohlcvHistorical']['quotes']
-#    listings = coin_data['props']['initialState']['cryptocurrency']['listingLatest']['data']
+link = 'https://coinmarketcap.com/currencies/bitcoin/historical-data/?start={}&end={}'
 
+def get_coinmarketcap_info(url,s_date,e_date):
+    response = requests.get(url.format(s_date,e_date))
+    soup = BeautifulSoup(response.text,"lxml")
 
-    market_cap = []
-    volume = []
-    timestamp = []
-    coin_name = []
-    coin_symbol = []
-    price = []
+    for items in soup.select("table.table tr.text-right"):
+        date = items.select_one("td.text-left").get_text(strip=True)
+        close = items.select_one("td[data-format-market-cap]").find_previous_sibling().get_text(strip=True)
+        volume = items.select_one("td[data-format-market-cap]").get_text(strip=True)
+        marketcap = items.select_one("td[data-format-market-cap]").find_next_sibling().get_text(strip=True)
+        yield date,close,volume,marketcap
 
-    for i in quotes:
-      coin_name.append(info['name'])
-      coin_symbol.append(info['symbol'])
-      market_cap.append(i['quote'][GBP]['market_cap'])
-      volume.append(i['quote'][GBP]['volume_24h'])
-      price.append(i['quote']['GBP']['price'])
-      timestamp.append(i['quote'][GBP]['timestamp'])
-    
-
-    df = pd.DataFrame(columns=['coin_name', 'coin_symbol', 'market_cap', 'volume', 'price', 'timestamp'])
-    df['coin_name'] = coin_name
-    df['coin_symbol'] = coin_symbol
-    df['price'] = price
-    df['market_cap'] = market_cap
-    df['volume'] = volume
-    df['timestamp'] = timestamp
-    return df
-
-df = load_data()
-
-st.write(df)
+if __name__ == '__main__':
+    dataframe = (elem for elem in get_coinmarketcap_info(link,s_date='20130428',e_date='20191020'))
+    df = pandas.DataFrame(dataframe)
+    print(df)
