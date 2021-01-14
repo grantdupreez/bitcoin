5bdc193e96fca49b179e5675f3f694147d5e9150
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-14 12:44:27 +0000

    Update price_tracker.py

--- price_tracker.py
+++ price_tracker.py
@@ -6,7 +6,7 @@ import requests
 import json
 import time
 
-currency_price_unit = sidebar.selectbox('Select currency for price', ('USD', 'BTC', 'ETH'))
+currency_price_unit = st.sidebar.selectbox('Select currency for price', ('USD', 'BTC', 'ETH'))
 
 def load_data():
     cmc = requests.get('https://coinmarketcap.com')
