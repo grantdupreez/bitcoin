96697f39cd696ea1c9c8e8d192a3d180292c1013
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-14 12:41:53 +0000

    Update price_tracker.py

--- price_tracker.py
+++ price_tracker.py
@@ -16,34 +16,5 @@ def load_data():
     data = soup.find('script', id='__NEXT_DATA__', type='application/json')
     historical_data = json.loads(data.contents[0])
     quotes = historical_data['props']['initialState']['cryptocurrency']['ohlcvHistorical']['quotes']
-#    listings = coin_data['props']['initialState']['cryptocurrency']['listingLatest']['data']
 
-
-    market_cap = []
-    volume = []
-    timestamp = []
-    coin_name = []
-    coin_symbol = []
-    price = []
-
-    for i in quotes:
-      coin_name.append(info['name'])
-      coin_symbol.append(info['symbol'])
-      market_cap.append(i['quote'][GBP]['market_cap'])
-      volume.append(i['quote'][GBP]['volume_24h'])
-      price.append(i['quote']['GBP']['price'])
-      timestamp.append(i['quote'][GBP]['timestamp'])
-    
-
-    df = pd.DataFrame(columns=['coin_name', 'coin_symbol', 'market_cap', 'volume', 'price', 'timestamp'])
-    df['coin_name'] = coin_name
-    df['coin_symbol'] = coin_symbol
-    df['price'] = price
-    df['market_cap'] = market_cap
-    df['volume'] = volume
-    df['timestamp'] = timestamp
-    return df
-
-df = load_data()
-
-st.write(df)
+    st.write(quotes)
