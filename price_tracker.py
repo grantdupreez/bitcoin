4026a77484f6904f1ed4121dcccc3a911c2c6d40
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-14 12:39:14 +0000

    Update price_tracker.py

--- price_tracker.py
+++ price_tracker.py
@@ -1,17 +1,49 @@
-import requests
-import lxml.html
+import streamlit as st
 import pandas as pd
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
+import base64
+from bs4 import BeautifulSoup
+import requests
+import json
+import time
 
-def get_date_range(number_of_months:int):
-    now = datetime.now()
-    dt_end = now.strftime("%Y%m%d")
-    dt_start = (now - relativedelta(months=number_of_months)).strftime("%Y%m%d")
-    return f'start={dt_start}&end={dt_end}'
+@st.cache
+def load_data():
+    url = "https://coinmarketcap.com/currencies/bitcoin/historical-data/?start="
+    start_date = "20210101&end="
+    end_date = "20210113"
+    cmc = requests.get(url + start_date + end_date)
+    soup = BeautifulSoup(cmc.content, 'html.parser')
+    data = soup.find('script', id='__NEXT_DATA__', type='application/json')
+    historical_data = json.loads(data.contents[0])
+    quotes = historical_data['props']['initialState']['cryptocurrency']['ohlcvHistorical']['quotes']
+#    listings = coin_data['props']['initialState']['cryptocurrency']['listingLatest']['data']
 
-number_of_months = 3
 
-table = pd.read_html(f'https://coinmarketcap.com/currencies/bitcoin/historical-data/?{get_date_range(number_of_months)}')[0]
-table = table[['Date', 'Close**', 'Volume','Market Cap']]
-print(table)
+    market_cap = []
+    volume = []
+    timestamp = []
+    coin_name = []
+    coin_symbol = []
+    price = []
+
+    for i in quotes:
+      coin_name.append(info['name'])
+      coin_symbol.append(info['symbol'])
+      market_cap.append(i['quote'][GBP]['market_cap'])
+      volume.append(i['quote'][GBP]['volume_24h'])
+      price.append(i['quote']['GBP']['price'])
+      timestamp.append(i['quote'][GBP]['timestamp'])
+    
+
+    df = pd.DataFrame(columns=['coin_name', 'coin_symbol', 'market_cap', 'volume', 'price', 'timestamp'])
+    df['coin_name'] = coin_name
+    df['coin_symbol'] = coin_symbol
+    df['price'] = price
+    df['market_cap'] = market_cap
+    df['volume'] = volume
+    df['timestamp'] = timestamp
+    return df
+
+df = load_data()
+
+st.write(df)
