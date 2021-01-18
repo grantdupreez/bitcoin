ebb126b5d6f682a29bd165f4f000b4f1fd8d1cee
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-18 17:21:35 +0000

    Update btc_tracker.py

--- btc_tracker.py
+++ btc_tracker.py
@@ -59,8 +59,8 @@ fig = go.Figure(data=[go.Candlestick(x=btc_df['Timestamp'],
                 low=btc_df['Low'],
                 close=btc_df['Close']), 
                   go.Scatter(x=btc_df['Timestamp'], y=btc_df['Close'], line=dict(color='orange', width=1)),
-                  go.Scatter(x=btc_df['Timestamp'], y=btc_df.['bollinger_mid_band'], line=dict(color='green', width=1))
-                  go.Scatter(x=btc_df['Timestamp'], y=btc_df.['bollinger_upper_band'], line=dict(color='red', width=1))
+                  go.Scatter(x=btc_df['Timestamp'], y=btc_df.['bollinger_mid_band'], line=dict(color='green', width=1)),
+                  go.Scatter(x=btc_df['Timestamp'], y=btc_df.['bollinger_upper_band'], line=dict(color='red', width=1)),
                   go.Scatter(x=btc_df['Timestamp'], y=btc_df.['bollinger_lower_band'], line=dict(color='blue', width=1))
             ])
 
