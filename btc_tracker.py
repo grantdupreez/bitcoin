299e8abefff1f5b6b080d3eb5499243cc1ec439d
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-18 22:42:55 +0000

    Update btc_tracker.py

--- btc_tracker.py
+++ btc_tracker.py
@@ -64,7 +64,7 @@ btc_df['crossover_long'] = np.where(btc_df['fast_close'] > btc_df['slow_close'],
 btc_df['crossover_short'] = np.where(btc_df['fast_close'] < btc_df['slow_close'], -1.0, 0.0)
 btc_df['crossover_signal'] = btc_df['crossover_long'] + btc_df['crossover_short']
 btc_df.head()
-st.write("Set short and long windows")
+#st.write("Set short and long windows")
 #btc_df
 
 
@@ -80,7 +80,7 @@ btc_df['bollinger_lower_band']  = btc_df['bollinger_mid_band'] - (btc_df['bollin
 btc_df['bollinger_long'] = np.where(btc_df['Close'] < btc_df['bollinger_lower_band'], 1.0, 0.0)
 btc_df['bollinger_short'] = np.where(btc_df['Close'] > btc_df['bollinger_upper_band'], -1.0, 0.0)
 btc_df['bollinger_signal'] = btc_df['bollinger_long'] + btc_df['bollinger_short']
-st.write("Set bollinger band window")
+#st.write("Set bollinger band window")
 #btc_df
 
 # Plot 
