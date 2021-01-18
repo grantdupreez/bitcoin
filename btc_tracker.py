95f47ea0786485a894fe23aa2eec7f347fa31a37
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-18 15:33:21 +0000

    Update btc_tracker.py

--- btc_tracker.py
+++ btc_tracker.py
@@ -47,10 +47,10 @@ btc_df['bollinger_signal'] = btc_df['bollinger_long'] + btc_df['bollinger_short'
 
 # Plot the Bollinger Bands for BTC/USD closing prices
 #fig = plt.figure()
-fig, ax = plt.subplots()
-ax = btc_df[['Close','bollinger_mid_band','bollinger_upper_band','bollinger_lower_band']].plot(figsize=(20,10))
+fig = plt.subplots()
+fix = btc_df[['Close','bollinger_mid_band','bollinger_upper_band','bollinger_lower_band']].plot(figsize=(20,10))
 
-plt.plot(fig)
+plt.plot()
 
 #plt.plot(
 #  btc_df[['Close','bollinger_mid_band','bollinger_upper_band','bollinger_lower_band']].plot(figsize=(20,10)))
