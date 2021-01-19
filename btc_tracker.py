6b8f76779746fe902092d4f5460160158697d1da
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-19 09:49:48 +0000

    Update btc_tracker.py

--- btc_tracker.py
+++ btc_tracker.py
@@ -66,9 +66,9 @@ if uploaded_file is not None:
                     low=btc_df['Low'],
                     close=btc_df['Close']), 
                       go.Scatter(x=btc_df.Timestamp, y=btc_df.Close, line=dict(color='orange', width=1)),
-                      go.Scatter(x=btc_df.Timestamp, y=btc_df.bollinger_mid_band, line=dict(color='green', width=1)),
-                      go.Scatter(x=btc_df.Timestamp, y=btc_df.bollinger_upper_band, line=dict(color='red', width=1)),
-                      go.Scatter(x=btc_df.Timestamp, y=btc_df.bollinger_lower_band, line=dict(color='blue', width=1)),
+                      go.Scatter(x=btc_df.Timestamp, y=btc_df.bollinger_mid_band, line=dict(color='green', width=1), name='Mid'),
+                      go.Scatter(x=btc_df.Timestamp, y=btc_df.bollinger_upper_band, line=dict(color='red', width=1), name='Upper'),
+                      go.Scatter(x=btc_df.Timestamp, y=btc_df.bollinger_lower_band, line=dict(color='blue', width=1), name='Lower'),
                 ])
     fig
 
