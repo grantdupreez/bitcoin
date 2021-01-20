c7ba7f2c72f36169b757d4114abd59f2ecafac01
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-20 15:05:33 +0000

    Update btc_tracker.py

--- btc_tracker.py
+++ btc_tracker.py
@@ -27,81 +27,81 @@ if uploaded_file is not None:
     btc_df = pd.read_csv(uploaded_file, header=[0])
     btc_df.head()
 
-  warnings.filterwarnings('ignore')
+    warnings.filterwarnings('ignore')
 
-  # Set index as datetime object and drop columns
-  btc_df.set_index(pd.to_datetime(btc_df['Timestamp'], infer_datetime_format=True), inplace=True)
-  btc_df.head()
+    # Set index as datetime object and drop columns
+    btc_df.set_index(pd.to_datetime(btc_df['Timestamp'], infer_datetime_format=True), inplace=True)
+    btc_df.head()
 
-  # Drop NAs and calculate daily percent return
-  btc_df['daily_return'] = btc_df['Close'].dropna().pct_change()
-  btc_df
+    # Drop NAs and calculate daily percent return
+    btc_df['daily_return'] = btc_df['Close'].dropna().pct_change()
+    btc_df
 
-  # Set short and long windows
-  short_window = 1
-  long_window = 10
-  st.write("Short window set to: 1 / Long window set to: 10")
-  # Construct a `Fast` and `Slow` Exponential Moving Average from short and long windows, respectively
-  btc_df['fast_close'] = btc_df['Close'].ewm(halflife=short_window).mean()
-  btc_df['slow_close'] = btc_df['Close'].ewm(halflife=long_window).mean()
-  # Construct a crossover trading signal
-  #    btc_df['crossover_long'] = np.where(btc_df['fast_close'] > btc_df['slow_close'], 1.0, 0.0)
-  #    btc_df['crossover_short'] = np.where(btc_df['fast_close'] < btc_df['slow_close'], -1.0, 0.0)
-  #    btc_df['crossover_signal'] = btc_df['crossover_long'] + btc_df['crossover_short']
-  btc_df['signal'] = np.where(btc_df['fast_close'] > btc_df['slow_close'], btc_df['Close'], None)
-  btc_df.head()
-  st.write("Set short and long windows")
-  btc_df
+    # Set short and long windows
+    short_window = 1
+    long_window = 10
+    st.write("Short window set to: 1 / Long window set to: 10")
+    # Construct a `Fast` and `Slow` Exponential Moving Average from short and long windows, respectively
+    btc_df['fast_close'] = btc_df['Close'].ewm(halflife=short_window).mean()
+    btc_df['slow_close'] = btc_df['Close'].ewm(halflife=long_window).mean()
+    # Construct a crossover trading signal
+    #    btc_df['crossover_long'] = np.where(btc_df['fast_close'] > btc_df['slow_close'], 1.0, 0.0)
+    #    btc_df['crossover_short'] = np.where(btc_df['fast_close'] < btc_df['slow_close'], -1.0, 0.0)
+    #    btc_df['crossover_signal'] = btc_df['crossover_long'] + btc_df['crossover_short']
+    btc_df['signal'] = np.where(btc_df['fast_close'] > btc_df['slow_close'], btc_df['Close'], None)
+    btc_df.head()
+    st.write("Set short and long windows")
+    btc_df
 
-  st.write("EMA of Closing prices")
-  fig = go.Figure()
-  fig.add_trace(go.Scatter(x=btc_df.Timestamp, y=btc_df['Close'],
+    st.write("EMA of Closing prices")
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=btc_df.Timestamp, y=btc_df['Close'],
                   mode='lines',
                   name='Close'))
-  fig.add_trace(go.Scatter(x=btc_df.Timestamp, y=btc_df.fast_close,
+    fig.add_trace(go.Scatter(x=btc_df.Timestamp, y=btc_df.fast_close,
                   mode='lines',
                   name='SMA = 1'))
-  fig.add_trace(go.Scatter(x=btc_df.Timestamp, y=btc_df.slow_close,
+    fig.add_trace(go.Scatter(x=btc_df.Timestamp, y=btc_df.slow_close,
                   mode='lines',
                   name='SMA = 10'))
-  if btc_df['signal'] is not None:
+    if btc_df['signal'] is not None:
       fig.add_trace(go.Scatter(x=btc_df.Timestamp, y=btc_df['Close'],
                   mode='markers',
                   name='Signal'))
 
-  fig
+    fig
 
-  st.write("EMA of Daily Return Volatility")
-  st.write("Exponential moving average (EMA) of the closing prices represents a moving average with more weight on the most recent of prices")
-  fig = go.Figure()
-  fig.add_trace(go.Scatter(x=btc_df.Timestamp, y=btc_df['slow_close'],
+    st.write("EMA of Daily Return Volatility")
+    st.write("Exponential moving average (EMA) of the closing prices represents a moving average with more weight on the most recent of prices")
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=btc_df.Timestamp, y=btc_df['slow_close'],
                   mode='lines',
                   name='Slow Close'))
-  fig.add_trace(go.Scatter(x=btc_df.Timestamp, y=btc_df.fast_close,
+    fig.add_trace(go.Scatter(x=btc_df.Timestamp, y=btc_df.fast_close,
                   mode='lines',
                   name='Fast Close'))
-  fig
+    fig
 
 
-  # Set bollinger band window
-  bollinger_window = 20
-  # Calculate rolling mean and standard deviation
-  btc_df['bollinger_mid_band'] = btc_df['Close'].rolling(window=bollinger_window).mean()
-  btc_df['bollinger_std'] = btc_df['Close'].rolling(window=20).std()
-  # Calculate upper and lowers bands of bollinger band
-  btc_df['bollinger_upper_band']  = btc_df['bollinger_mid_band'] + (btc_df['bollinger_std'] * 1)
-  btc_df['bollinger_lower_band']  = btc_df['bollinger_mid_band'] - (btc_df['bollinger_std'] * 1)
-  # Calculate bollinger band trading signal
-  btc_df['bollinger_long'] = np.where(btc_df['Close'] < btc_df['bollinger_lower_band'], 1.0, 0.0)
-  btc_df['bollinger_short'] = np.where(btc_df['Close'] > btc_df['bollinger_upper_band'], -1.0, 0.0)
-  btc_df['bollinger_signal'] = btc_df['bollinger_long'] + btc_df['bollinger_short']
-  st.write("Set bollinger band window")
-  btc_df
+    # Set bollinger band window
+    bollinger_window = 20
+    # Calculate rolling mean and standard deviation
+    btc_df['bollinger_mid_band'] = btc_df['Close'].rolling(window=bollinger_window).mean()
+    btc_df['bollinger_std'] = btc_df['Close'].rolling(window=20).std()
+    # Calculate upper and lowers bands of bollinger band
+    btc_df['bollinger_upper_band']  = btc_df['bollinger_mid_band'] + (btc_df['bollinger_std'] * 1)
+    btc_df['bollinger_lower_band']  = btc_df['bollinger_mid_band'] - (btc_df['bollinger_std'] * 1)
+    # Calculate bollinger band trading signal
+    btc_df['bollinger_long'] = np.where(btc_df['Close'] < btc_df['bollinger_lower_band'], 1.0, 0.0)
+    btc_df['bollinger_short'] = np.where(btc_df['Close'] > btc_df['bollinger_upper_band'], -1.0, 0.0)
+    btc_df['bollinger_signal'] = btc_df['bollinger_long'] + btc_df['bollinger_short']
+    st.write("Set bollinger band window")
+    btc_df
 
-  # Plot 
-  st.write("Bollinger Bands")
+    # Plot 
+    st.write("Bollinger Bands")
 
-  fig = go.Figure(data=[go.Candlestick(x=btc_df['Timestamp'],
+    fig = go.Figure(data=[go.Candlestick(x=btc_df['Timestamp'],
                   open=btc_df['Open'],
                   high=btc_df['High'],
                   low=btc_df['Low'],
@@ -112,15 +112,15 @@ if uploaded_file is not None:
                     go.Scatter(x=btc_df.Timestamp, y=btc_df.bollinger_lower_band, line=dict(color='blue', width=1), name='Lower'),
               ])
 
-  #    fig.add_trace(go.Scatter(x=btc_df.Timestamp, y=btc_df.signal,
-  #                    mode='markers',
-  #                    name='Signal'))
-  fig
+    #    fig.add_trace(go.Scatter(x=btc_df.Timestamp, y=btc_df.signal,
+    #                    mode='markers',
+    #                    name='Signal'))
+    fig
 
-  st.write("Volume")
-  fig = px.histogram(btc_df, x="Timestamp", y="Volume")
-  fig
+    st.write("Volume")
+    fig = px.histogram(btc_df, x="Timestamp", y="Volume")
+    fig
 
-  st.write("Market Cap")
-  fig = px.histogram(btc_df, x="Timestamp", y="Market_Cap")
-  fig
+    st.write("Market Cap")
+    fig = px.histogram(btc_df, x="Timestamp", y="Market_Cap")
+    fig
