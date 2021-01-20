e218f326563e598b9817ff7d3e715affb68e4bfb
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-20 21:23:54 +0000

    Update btc_tracker.py

--- btc_tracker.py
+++ btc_tracker.py
@@ -20,7 +20,7 @@ start_date = st.sidebar.date_input("Start Date", st_date)
 
 y_df = BTC_Data.history(start=start_date)
 mark_cap_df = BTC_Data.info("marketCap")
-st.write("Market capitalisation: " + mark_cap_df
+st.write("Market capitalisation: " + mark_cap_df)
 
 y_df = y_df.reset_index()
 for i in ['Open', 'High', 'Close', 'Low']: 
