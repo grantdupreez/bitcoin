dd40c6b1f7ab9075b1bb2972eca9f94b0043a2ad
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-22 17:03:05 +0000

    Update btc_daily.py

--- btc_daily.py
+++ btc_daily.py
@@ -32,7 +32,7 @@ btc_df['bollinger_lower_band']  = btc_df['bollinger_mid_band'] - (btc_df['bollin
 btc_df['bollinger_long'] = np.where(btc_df['Close'] < btc_df['bollinger_lower_band'], 1.0, 0.0)
 btc_df['bollinger_short'] = np.where(btc_df['Close'] > btc_df['bollinger_upper_band'], -1.0, 0.0)
 btc_df['bollinger_signal'] = btc_df['bollinger_long'] + btc_df['bollinger_short']
-st.write("Set bollinger band window - window:" + bollinger_window)
+st.write("Set bollinger band window - window:" + str(bollinger_window))
 
 btc_df
 
