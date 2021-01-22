55902342636bc3cf596f715f3bfc3773611d73c0
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-22 09:05:42 +0000

    Update btc_tracker.py

--- btc_tracker.py
+++ btc_tracker.py
@@ -22,7 +22,7 @@ start_date = st.sidebar.date_input("Start Date", st_date)
 #to_date = str(today)
 to_date = f'{datetime.now():%Y-%m-%d}'
 
-y_df = BTC_Data.history(start=start_date, end=to_date, interval="1h")
+y_df = BTC_Data.history(start=start_date, end=to_date, interval="1d")
 #y_df = BTC_Data.history(start=start_date, end=to_date)
 
 st.write("Market capitalisation: " + str(Money(BTC_Data.info["marketCap"], 'USD')))
