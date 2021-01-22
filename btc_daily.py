893b92b6022fad3b8c7d6a2a601f72db2472f4fd
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-22 15:44:48 +0000

    Update btc_daily.py

--- btc_daily.py
+++ btc_daily.py
@@ -37,57 +37,8 @@ btc_df.head()
 
 # Drop NAs and calculate daily percent return
 btc_df['daily_return'] = btc_df['Close'].dropna().pct_change()
-#btc_df
 
 signals_df = btc_df.drop(columns=['Open', 'High', 'Low', 'Volume','Dividends', 'Stock Splits'])
 
-# Set the short window and long windows
-short_window = 50
-long_window = 100
-# Generate the short and long moving averages (50 and 100 days, respectively)
-signals_df['SMA50'] = signals_df['Close'].rolling(window=short_window).mean()
-signals_df['SMA100'] = signals_df['Close'].rolling(window=long_window).mean()
-signals_df['Signal'] = 0.0
-# Generate the trading signal 0 or 1,
-# where 0 is when the SMA50 is under the SMA100, and
-# where 1 is when the SMA50 is higher (or crosses over) the SMA100
-signals_df['Signal'][short_window:] = np.where(
-    signals_df['SMA50'][short_window:] > signals_df['SMA100'][short_window:], 1.0, 0.0
-)
-# Calculate the points in time at which a position should be taken, 1 or -1
-signals_df['Entry/Exit'] = signals_df['Signal'].diff()
-# Print the DataFrame
-signals_df.tail(10)
+signals_df
 
-# Visualize exit position relative to close price
-exit = signals_df[signals_df['Entry/Exit'] == -1.0]['Close'].hvplot.scatter(
-    color='red',
-    legend=False,
-    ylabel='Price in $',
-    width=1000,
-    height=400
-)
-# Visualize entry position relative to close price
-entry = signals_df[signals_df['Entry/Exit'] == 1.0]['Close'].hvplot.scatter(
-    color='green',
-    legend=False,
-    ylabel='Price in $',
-    width=1000,
-    height=400
-)
-# Visualize close price for the investment
-security_close = signals_df[['Close']].hvplot(
-    line_color='lightgray',
-    ylabel='Price in $',
-    width=1000,
-    height=400
-)
-# Visualize moving averages
-moving_avgs = signals_df[['SMA50', 'SMA100']].hvplot(
-    ylabel='Price in $',
-    width=1000,
-    height=400
-)
-# Overlay plots
-entry_exit_plot = security_close * moving_avgs * entry * exit
-entry_exit_plot.opts(xaxis=None)
