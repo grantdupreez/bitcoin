5077b6cfd23462aa49bd91cbac48c319b0c9df61
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-24 09:21:56 +0000

    Update btc_daily.py

--- btc_daily.py
+++ btc_daily.py
@@ -40,7 +40,9 @@ btc_df['bollinger_signal'] = np.where(btc_df['bollinger_long'] + btc_df['bolling
 st.write("Set bollinger band window - window:" + str(select_window))
 
 mc = yf.Ticker(select_currency)
-st.write("Market capitalisation: " + str(Money(mc.info["marketCap"], 'GBP')))
+cur = select_currency
+cur = cur[-3:]
+st.write("Market capitalisation: " + str(Money(mc.info["marketCap"], cur)))
 
 fig = go.Figure(data=[go.Candlestick(x=btc_df['Datetime'],
             open=btc_df['Open'],
