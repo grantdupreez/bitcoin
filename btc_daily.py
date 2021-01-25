9fd9346d0f05d93ff4e3709d6fd7e363d7339841
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-25 22:07:38 +0000

    Update btc_daily.py

--- btc_daily.py
+++ btc_daily.py
@@ -30,6 +30,7 @@ select_signals = st.sidebar.checkbox('Show signals?')
 #btc_df = yf.download(tickers=select_currency, period=select_period, interval=select_interval)
 btc_df = yf.download(tickers=select_currency, start=start_date, end=to_date, interval=select_interval) 
 
+btc_df
 
 btc_df = btc_df.reset_index()
 for i in ['Open', 'High', 'Close', 'Low']: 
@@ -51,22 +52,3 @@ cur = cur[-3:]
 st.write("Market capitalisation: " + str(Money(mc.info["marketCap"], cur)))
 st.write("Bollinger band window:" + str(select_window))
 
-fig = go.Figure(data=[go.Candlestick(x=btc_df['Datetime'],
-            open=btc_df['Open'],
-            high=btc_df['High'],
-            low=btc_df['Low'],
-            close=btc_df['Close']), 
-              go.Scatter(x=btc_df.Datetime, y=btc_df.Close, line=dict(color='grey', width=1), name='Close'),
-              go.Scatter(x=btc_df.Datetime, y=btc_df.bollinger_mid_band, line=dict(color='orange', width=1), name='Mid'),
-              go.Scatter(x=btc_df.Datetime, y=btc_df.bollinger_upper_band, line=dict(color='red', width=1), name='Upper'),
-              go.Scatter(x=btc_df.Datetime, y=btc_df.bollinger_lower_band, line=dict(color='blue', width=1), name='Lower')
-                     ])
-if select_signals:
-      fig.add_trace(go.Scatter(x=btc_df.Datetime, y=btc_df.bollinger_signal, mode='markers', line=dict(color='black', width=1), name='Signal'))
-
-fig
-
-fig = px.bar(btc_df, x="Datetime", y="Volume")
-fig
-
-btc_df
