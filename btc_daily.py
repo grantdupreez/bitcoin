67e9128e7e8cc87c4ebaa7c1cc8fdd363db0f54e
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-25 22:15:30 +0000

    Update btc_daily.py

--- btc_daily.py
+++ btc_daily.py
@@ -1,4 +1,3 @@
-# Import libraries and dependencies
 import pandas as pd
 import streamlit as st
 import numpy as np
@@ -13,32 +12,18 @@ from money import Money
 
 st.title("Bitcoin Daily Analysis")
 
-
 select_currency = st.sidebar.selectbox('Select currency?', ('BTC-GBP','BTC-USD'))
-#select_period = st.sidebar.selectbox('Select period?', ('10d','5d','1d'))
+select_period = st.sidebar.selectbox('Select period?', ('10d','5d','1d'))
 #select_interval = st.sidebar.selectbox('Select interval?', ('90m','60m','30m','15m','5m','2m','1m'))
-
-today = datetime.today()
-st_date = today - timedelta(days=60)
-start_date = st.sidebar.date_input("Start Date", st_date)
-to_date = f'{datetime.now():%Y-%m-%d}'
-
 select_interval = st.sidebar.selectbox('Select interval?', ('90m','60m','30m','15m','5m'))
 select_window = st.sidebar.slider('Set window', min_value=10, max_value=50, value=20, step=5)
 select_signals = st.sidebar.checkbox('Show signals?')
-
-#btc_df = yf.download(tickers=select_currency, period=select_period, interval=select_interval)
-btc_df = yf.download(tickers=select_currency, interval=select_interval) 
-
-st.write(to_date)
-st.write(start_date)
-
-btc_df
+btc_df = yf.download(tickers=select_currency, period=select_period, interval=select_interval)
 
 btc_df = btc_df.reset_index()
 for i in ['Open', 'High', 'Close', 'Low']: 
       btc_df[i]  =  btc_df[i].astype('float64')
-
+      
 #bollinger_window = 10
 btc_df['bollinger_mid_band'] = btc_df['Close'].rolling(window=select_window).mean()
 btc_df['bollinger_std'] = btc_df['Close'].rolling(window=select_window).std()
@@ -46,7 +31,6 @@ btc_df['bollinger_upper_band']  = btc_df['bollinger_mid_band'] + (btc_df['bollin
 btc_df['bollinger_lower_band']  = btc_df['bollinger_mid_band'] - (btc_df['bollinger_std'] * 1)
 btc_df['bollinger_long'] = np.where(btc_df['Close'] < btc_df['bollinger_lower_band'], 1.0, 0.0)
 btc_df['bollinger_short'] = np.where(btc_df['Close'] > btc_df['bollinger_upper_band'], -1.0, 0.0)
-
 btc_df['bollinger_signal'] = np.where(btc_df['bollinger_long'] + btc_df['bollinger_short'] > 0, btc_df['Close'], None)
 
 mc = yf.Ticker(select_currency)
@@ -55,3 +39,22 @@ cur = cur[-3:]
 st.write("Market capitalisation: " + str(Money(mc.info["marketCap"], cur)))
 st.write("Bollinger band window:" + str(select_window))
 
+fig = go.Figure(data=[go.Candlestick(x=btc_df['Datetime'],
+            open=btc_df['Open'],
+            high=btc_df['High'],
+            low=btc_df['Low'],
+            close=btc_df['Close']), 
+              go.Scatter(x=btc_df.Datetime, y=btc_df.Close, line=dict(color='grey', width=1), name='Close'),
+              go.Scatter(x=btc_df.Datetime, y=btc_df.bollinger_mid_band, line=dict(color='orange', width=1), name='Mid'),
+              go.Scatter(x=btc_df.Datetime, y=btc_df.bollinger_upper_band, line=dict(color='red', width=1), name='Upper'),
+              go.Scatter(x=btc_df.Datetime, y=btc_df.bollinger_lower_band, line=dict(color='blue', width=1), name='Lower')
+                     ])
+
+if select_signals:
+      fig.add_trace(go.Scatter(x=btc_df.Datetime, y=btc_df.bollinger_signal, mode='markers', line=dict(color='black', width=1), name='Signal'))
+fig
+
+fig = px.bar(btc_df, x="Datetime", y="Volume")
+fig
+
+btc_df
