79cab7d43a56c866d7af4582ad972455a88d5008
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-26 08:25:46 +0000

    Update yf.py

--- yf.py
+++ yf.py
@@ -1,5 +1,8 @@
 import yfinance as yf
+from pandas_datareader import data as pdr
 
-bc = yf.Ticker("BTC-GBP")
-df = yf.download("BTC-GBP", start="2017-01-01", end="2021-01-26")
+yf.pdr_override()
+#bc = yf.Ticker("BTC-GBP")
+#df = yf.download("BTC-GBP", start="2017-01-01", end="2021-01-26")
+df = pdr.get_data_yahoo("BTC-GBP", start="2021-01-18", end="2021-01-26")
 df
