bfd631bfe97e08e096bbd3061c2bdf9937e0e785
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-01-26 08:53:58 +0000

    Update yf.py

--- yf.py
+++ yf.py
@@ -1,16 +1,4 @@
-import requests
-import pandas as pd
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
+import yfinance as yf
 
-def get_date_range(number_of_months:int):
-    now = datetime.now()
-    dt_end = now.strftime("%Y%m%d")
-    dt_start = (now - relativedelta(months=number_of_months)).strftime("%Y%m%d")
-    return f'start={dt_start}&end={dt_end}'
-
-number_of_months = 3
-
-table = pd.read_html(f'https://coinmarketcap.com/currencies/bitcoin/historical-data/?{get_date_range(number_of_months)}')[0]
-table = table[['Date', 'Close**', 'Volume','Market Cap']]
-table
+btc = yf.Ticker("BTC-GBP")
+btc
