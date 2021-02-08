f9e04480ecf50cd80e68ab6e7d10bfcdf0e025a8
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-02-08 11:02:33 +0000

    Create btc_daily.py

--- btc_daily.py
+++ btc_daily.py
@@ -18,8 +18,10 @@ select_period = st.sidebar.selectbox('Select period?', ('10d','5d','1d'))
 #select_interval = st.sidebar.selectbox('Select interval?', ('90m','60m','30m','15m','5m','2m','1m'))
 select_interval = st.sidebar.selectbox('Select interval?', ('90m','60m','30m','15m','5m'))
 select_window = st.sidebar.slider('Set window', min_value=10, max_value=50, value=20, step=5)
-select_short = st.sidebar.slider('Set SMA', min_value=1, max_value=20, value=10, step=1)
-select_long = st.sidebar.slider('Set SMA', min_value=0, max_value=100, value=40, step=10)
+select_short = st.sidebar.slider('Set short MA', min_value=1, max_value=20, value=10, step=1)
+select_long = st.sidebar.slider('Set long MA', min_value=1, max_value=100, value=40, step=1)
+select_vol_short = st.sidebar.slider('Set volume short window', min_value=1, max_value=20, value=10, step=1)
+select_vol_long = st.sidebar.slider('Set volume long window', min_value=1, max_value=100, value=40, step=1)
 select_signals = st.sidebar.checkbox('Show signals?')
 select_close = st.sidebar.checkbox('Show closing tracker?')
 select_bollinger = st.sidebar.checkbox('Show Bollinger bands?')
@@ -31,7 +33,8 @@ btc_df = yf.download(tickers=select_currency, period=select_period, interval=sel
 btc_df = btc_df.reset_index()
 for i in ['Open', 'High', 'Close', 'Low']: 
       btc_df[i]  =  btc_df[i].astype('float64')
-      
+
+#SET TRADING SIGNAL 1 - BOLLINGER BANDS
 btc_df['bollinger_mid_band'] = btc_df['Close'].rolling(window=select_window).mean()
 btc_df['bollinger_std'] = btc_df['Close'].rolling(window=select_window).std()
 btc_df['bollinger_upper_band']  = btc_df['bollinger_mid_band'] + (btc_df['bollinger_std'] * 1)
@@ -40,13 +43,29 @@ btc_df['bollinger_long'] = np.where(btc_df['Close'] < btc_df['bollinger_lower_ba
 btc_df['bollinger_short'] = np.where(btc_df['Close'] > btc_df['bollinger_upper_band'], -1.0, 0.0)
 btc_df['bollinger_signal'] = np.where(btc_df['bollinger_long'] + btc_df['bollinger_short'] > 0, btc_df['Close'], None)
 
+#SET TRADING SIGNAL 2 - EMA
 # Set short and long windows
 short_window = select_short
 long_window = select_long
 # Construct a `Fast` and `Slow` Exponential Moving Average from short and long windows, respectively
 btc_df['fast_close'] = btc_df['Close'].ewm(halflife=select_short).mean()
 btc_df['slow_close'] = btc_df['Close'].ewm(halflife=select_long).mean()
-btc_df['ema_signal'] = np.where(btc_df['fast_close'] == btc_df['slow_close'], btc_df['Close'], None)
+#btc_df['ema_signal'] = np.where(btc_df['fast_close'] == btc_df['slow_close'], btc_df['Close'], None)
+
+# Construct a crossover trading signal
+btc_df['crossover_long'] = np.where(btc_df['fast_close'] > btc_df['slow_close'], 1.0, 0.0)
+btc_df['crossover_short'] = np.where(btc_df['fast_close'] < btc_df['slow_close'], -1.0, 0.0)
+btc_df['crossover_signal'] = btc_df['crossover_long'] + btc_df['crossover_short']
+
+#SET TRADING SIGNAL 3 - EMA
+# Construct a `Fast` and `Slow` Exponential Moving Average from short and long windows, respectively
+btc_df['fast_vol'] = btc_df['daily_return'].ewm(halflife=select_vol_short).std()
+btc_df['slow_vol'] = btc_df['daily_return'].ewm(halflife=select_vol_long).std()
+# Construct a crossover trading signal
+btc_df['vol_trend_long'] = np.where(btc_df['fast_vol'] < btc_df['slow_vol'], 1.0, 0.0)
+btc_df['vol_trend_short'] = np.where(btc_df['fast_vol'] > btc_df['slow_vol'], -1.0, 0.0) 
+btc_df['vol_trend_signal'] = btc_df['vol_trend_long'] + btc_df['vol_trend_short']
+
 
 mc = yf.Ticker(select_currency)
 cur = select_currency
