b4f1c30784d4773e006fadf653e9db6a425aaee6
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-02-08 10:23:21 +0000

    Update btc_daily.py

--- btc_daily.py
+++ btc_daily.py
@@ -18,8 +18,13 @@ select_period = st.sidebar.selectbox('Select period?', ('10d','5d','1d'))
 #select_interval = st.sidebar.selectbox('Select interval?', ('90m','60m','30m','15m','5m','2m','1m'))
 select_interval = st.sidebar.selectbox('Select interval?', ('90m','60m','30m','15m','5m'))
 select_window = st.sidebar.slider('Set window', min_value=10, max_value=50, value=20, step=5)
+select_short = st.sidebar.slider('Set SMA', min_value=2, max_value=20, value=10, step=1)
+select_long = st.sidebar.slider('Set SMA', min_value=2, max_value=100, value=20, step=5)
 select_signals = st.sidebar.checkbox('Show signals?')
 select_close = st.sidebar.checkbox('Show closing tracker?')
+select_bollinger = st.sidebar.checkbox('Show Bollinger bands?')
+select_sma = st.sidebar.checkbox('Show Exponential Moving Averages?')
+
 
 btc_df = yf.download(tickers=select_currency, period=select_period, interval=select_interval)
 
@@ -36,6 +41,14 @@ btc_df['bollinger_long'] = np.where(btc_df['Close'] < btc_df['bollinger_lower_ba
 btc_df['bollinger_short'] = np.where(btc_df['Close'] > btc_df['bollinger_upper_band'], -1.0, 0.0)
 btc_df['bollinger_signal'] = np.where(btc_df['bollinger_long'] + btc_df['bollinger_short'] > 0, btc_df['Close'], None)
 
+# Set short and long windows
+short_window = select_short
+long_window = select_long
+# Construct a `Fast` and `Slow` Exponential Moving Average from short and long windows, respectively
+btc_df['fast_close'] = btc_df['Close'].ewm(halflife=select_short).mean()
+btc_df['slow_close'] = btc_df['Close'].ewm(halflife=select_long).mean()
+btc_df['ema_signal'] = np.where(btc_df['fast_close'] > btc_df['slow_close'], btc_df['Close'], None)
+
 mc = yf.Ticker(select_currency)
 cur = select_currency
 cur = cur[-3:]
@@ -46,11 +59,29 @@ fig = go.Figure(data=[go.Candlestick(x=btc_df['Datetime'],
             open=btc_df['Open'],
             high=btc_df['High'],
             low=btc_df['Low'],
-            close=btc_df['Close']), 
-              go.Scatter(x=btc_df.Datetime, y=btc_df.bollinger_mid_band, line=dict(color='orange', width=1), name='Mid'),
-              go.Scatter(x=btc_df.Datetime, y=btc_df.bollinger_upper_band, line=dict(color='red', width=1), name='Upper'),
-              go.Scatter(x=btc_df.Datetime, y=btc_df.bollinger_lower_band, line=dict(color='blue', width=1), name='Lower')
+            close=btc_df['Close'])
+#              go.Scatter(x=btc_df.Datetime, y=btc_df.bollinger_mid_band, line=dict(color='orange', width=1), name='Mid'),
+#              go.Scatter(x=btc_df.Datetime, y=btc_df.bollinger_upper_band, line=dict(color='red', width=1), name='Upper'),
+#              go.Scatter(x=btc_df.Datetime, y=btc_df.bollinger_lower_band, line=dict(color='blue', width=1), name='Lower')
                      ])
+
+if select_bollinger:
+      fig.add_trace(go.Scatter(x=btc_df.Datetime, y=btc_df.bollinger_mid_band, line=dict(color='orange', width=1), name='Mid'))
+      fig.add_trace(go.Scatter(x=btc_df.Datetime, y=btc_df.bollinger_upper_band, line=dict(color='red', width=1), name='Upper'))
+      fig.add_trace(go.Scatter(x=btc_df.Datetime, y=btc_df.bollinger_lower_band, line=dict(color='blue', width=1), name='Lower'))
+
+if select_sma:
+      fig.add_trace(go.Scatter(x=btc_df.Datetime, y=btc_df.fast_close,
+                  mode='lines',
+                  name='Short = ' + str(select_short)))
+      fig.add_trace(go.Scatter(x=btc_df.Datetime, y=btc_df.slow_close,
+                  mode='lines',
+                  name='Long = ' + str(select_long)))
+      if btc_df['ema_signal'] is not None:
+            fig.add_trace(go.Scatter(x=btc_df.Datetime, y=btc_df['Close'],
+                  mode='markers',
+                  name='Signal'))
+      
 
 if select_signals:
       fig.add_trace(go.Scatter(x=btc_df.Datetime, y=btc_df.bollinger_signal, mode='markers', line=dict(color='black', width=1), name='Signal'))
